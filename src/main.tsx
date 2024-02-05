645d82016851ca95f87abce7e434be5d44992daa
Zhang-Haoran <haoranzhang001@gmail.com>
2024-02-05 23:02:11 +1100

    uninstall styled-components & install tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
-const root = document.getElementById('root')
-ReactDOM.render(<App />, root)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
